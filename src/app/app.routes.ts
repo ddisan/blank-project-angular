21b24b7e36914e9a16eff1cbcc0d6de7025bed28
ddisan <apisitn9244@gmail.com>
2025-06-19 23:36:38 +0700

    add example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./programs/example/example.routes').then((r) => r.routes),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/programs/example/example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './example.component.html',
+  styles: ``,
+})
+export class ExampleComponent {
+  title = 'blank-project-angular';
+}
